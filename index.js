5c40910450df0baa39a0936ecfa203fcc694e4f3
sanitago <santiago.martinez.rosa@gmail.com>
2020-11-18 12:56:26 +0100

    commit5

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+require('./db_examinator/mongoose');
 const port = process.env.PORT || 3000
 
+//
 app.listen(port, () => {
     console.log(`Server listening to port ${port}`)
   });
-
-app.use(express.static('public'));
+  
+//
+  app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
