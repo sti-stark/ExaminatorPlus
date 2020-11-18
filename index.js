0396c917a7d7278a3464f7d42f16ad178047b16f
sanitago <santiago.martinez.rosa@gmail.com>
2020-11-18 11:08:37 +0100

    commit3

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-});
+app.listen(port, () => {
+    console.log(`Server listening to port ${port}`)
+  });
 
 app.use(express.static('public'));
 
